da763709bc2c461d1afbb57314c677f792aa4113
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-05-25 19:18:54 +0100

    Funçoes principais de validação das credenciais foram movidas para fora do componente, de modo a poderem ser exportadas e testadas respectivamente.
    Testes validam a funcionalidade das funções exportadas.
    Problemas a testar chamadas de funções dentro de chamadas a submits
    ou outros componentes. Ainda a tentar perceber como funcionar com
    jest.fn, jest.spyOn e jest snapshot.

--- src/pages/Registry/index.js
+++ src/pages/Registry/index.js
@@ -2,19 +2,95 @@ import {React, useState} from 'react';
 import { TextField, Button, Box, Typography, Container} from "@mui/material";
 import {useNavigate} from 'react-router-dom'
 
+
+/**funções para exportar, ficam mais facilmente testaveis */
+//falta colocar aqui todas as funções que tratam da 
+export function passwordConfirmation(password, confirmPassword){
+    if (password !== confirmPassword){
+            return false
+        }
+        return true;
+}
+
+//handles email validation
+ export function emailValidation(email){
+    if (!email || email.length === 0 || !/@/.test(email) || /\s/.test(email)){
+        switch(true){
+            case !email:
+                console.log('email is empty or undefined');
+                return false;
+            case email.length === 0:
+                console.log('email length is 0')
+                return false;
+            case !/@/.test(email):
+                console.log("there is no @ in your email");
+                return false;
+            case /\s/.test(email):
+                console.log('spaces are not allowed in the email');
+                return false;
+            default:
+                console.log("there are other validation errors in your email");
+                return false;
+        }
+    }
+    else {
+        return true;
+    }
+}
+//handles password validation (rules for password input, not comparison with the confirm password field)
+export function passwordValidation(password){
+    if (!password || password.length === 0 || password.length < 8 /*|| !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/\d/.test(password) || !/[^A-Za-z0-9]/.test(password)*/){
+        switch (true){
+            case password.length === 0:
+                console.log('your password is undefined')
+                return false;
+            case password.length < 8:
+                console.log('your password length does not have minimum 8 chars')
+                return false;
+          /*
+        case !/[A-Z]/.test(password):
+          setInvalidPassword(true);
+          console.log('your password must contain an uppercase letter');
+          break;
+        case !/[a-z]/.test(password):
+            setInvalidPassword(true);
+          console.log('your password must contain at least one lower case letter');
+          break;
+        case !/\d/.test(password):
+            setInvalidPassword(true);
+          console.log('your password must contain at least one digit');
+          break;
+        case !/[^A-Za-z0-9]/.test(password):
+            setInvalidPassword(true);
+          console.log('your password must contain at least one special character');
+          break;
+          */
+            default: console.log('your password is wrong')
+        }
+    }
+      return true;
+  }
+//function that wraps together all verifications to allow a user to register
+export function credentialConfirmation(password, confirmPassword, email){
+    if (passwordConfirmation(password, confirmPassword) && emailValidation(email) && passwordValidation(password)){
+        return true;
+    }
+    else 
+        return false;
+}
+
+/**COMPONENTE */
 const RegistryForm = () =>{
     const navigate = useNavigate();
-    //these values are all that are required for the registry of a user. More details they can complete their profile when they log in
-    //navigate to login page
-    //navigate to login on register
+    //user object that will be sent to backend
     const [user, setUser] = useState({
         firstName:'',
         lastName:'',
         username:'',
         email:'',
         password:'',
-        //aqui falta o local onde trabalha
     });
+
     //NAVEGAÇÂO
     const goToLogin =()=>{
         console.log("button clicked")
@@ -23,115 +99,19 @@ const RegistryForm = () =>{
 
     //confirmação password
     const [confirmPassword, setConfirmPassword] = useState('');
-    const [passwordMatchError, setPasswordMatchError] = useState(false);
-    const [invalidPassword, setInvalidPassword]= useState(false);
-    //confirmação email
-    const [emailInputError, setEmailInputError] = useState(false);
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     //handle inputs for registry
     const handleChange = (e) => {
         const { name, value } = e.target;
         setUser({ ...user, [name]: value });
     };
+
     //handle input in the confirm password field
     const handleConfirmPassword = (e)=>{
         const {value} = e.target;
         setConfirmPassword(value);
     }
-    //handles email validation
-    const emailValidation=(email)=>{
-        if (!email || email.length === 0 || !/@/.test(email) || /\s/.test(email)){
-          switch(true){
-            case !email:
-              console.log('email is empty or undefined');
-              setEmailInputError(true);
-              break;
-            case email.length === 0:
-              console.log('email length is 0')
-              setEmailInputError(true);
-              break;
-            case !/@/.test(email):
-              console.log("there is no @ in your email");
-              setEmailInputError(true);
-              break;
-            case /\s/.test(email):
-              console.log('spaces are not allowed in the email');
-              setEmailInputError(true);
-              break;
-            default:
-              console.log("there are other validation errors in your email");
-              setEmailInputError(true);
-          }
-        }
-        else {
-            setEmailInputError(false);
-          return true;
-        }
-      }
-
-      //handles password validation
-      //implement a pop-over/some sourt of visual guide to display password rules
-      const passwordValidation =(password)=>{
-        if (!password || password.length === 0 || password.length < 8 /*|| !/[A-Z]/.test(password) || !/[a-z]/.test(password) || !/\d/.test(password) || !/[^A-Za-z0-9]/.test(password)*/){
-          switch (true){
-            case password.length === 0:
-                setInvalidPassword(true);
-              console.log('your password is undefined')
-              return false;
-              break;
-              //isto nao faz muito sentido, redundante
-            case password.length < 8:
-                setInvalidPassword(true);
-              console.log('your password length does not have minimum 8 chars')
-              return false;
-              break;
-
-              /*
-            case !/[A-Z]/.test(password):
-              setInvalidPassword(true);
-              console.log('your password must contain an uppercase letter');
-              break;
-            case !/[a-z]/.test(password):
-                setInvalidPassword(true);
-              console.log('your password must contain at least one lower case letter');
-              break;
-            case !/\d/.test(password):
-                setInvalidPassword(true);
-              console.log('your password must contain at least one digit');
-              break;
-            case !/[^A-Za-z0-9]/.test(password):
-                setInvalidPassword(true);
-              console.log('your password must contain at least one special character');
-              break;
-              */
-            default: console.log('your password is wrong')
-          }
-        }
-          setInvalidPassword(false);
-          return true;
-      }
-      
-      //handles comparison between password and confirm password field
-      const passwordConfirmation=(password, confirmPassword)=>{
-        if (password !== confirmPassword){
-            setPasswordMatchError(true);
-            return console.log("password error");
-        }
-        setPasswordMatchError(false);
-        return true;
-      }
-
-
-      //function that verifies if everything is true    
-      const credentialConfirmation = ()=>{
-        if (passwordConfirmation(user.password, confirmPassword) && emailValidation(user.email) && passwordValidation(user.password)){
-            return true;
-        }
-        else 
-        return false;
-
-      }
-
     /**
      * handle registry credentials
      * if password does not match, return something (can be a function)
@@ -142,6 +122,7 @@ const RegistryForm = () =>{
      */
     const handleSubmit = (event) => {
         event.preventDefault();
+        setFormSubmitted(true);
         if (credentialConfirmation()){
             goToLogin();
             console.log('user sucessfully registered')
@@ -214,8 +195,9 @@ const RegistryForm = () =>{
                         name="email"
                         value={user.email}
                         onChange={handleChange}
-                        error = {emailInputError}
-                        helperText ={emailInputError && "invalid email"}  
+                        //email confirmation
+                        error = {formSubmitted && !emailValidation(user.email)}
+                        helperText ={formSubmitted && !emailValidation(user.email) &&  "invalid email"}  
                         inputProps={{
                             "data-testid":"email-input"
                         }}
@@ -230,8 +212,9 @@ const RegistryForm = () =>{
                         type="password"
                         value={user.password}
                         onChange={handleChange}
-                        error={invalidPassword}
-                        helperText = {invalidPassword && "invalid password"}
+                        //password validation
+                        error={formSubmitted && !passwordValidation(user.password)}
+                        helperText = {!passwordValidation(user.password) && formSubmitted && "invalid password"}
                         inputProps={{
                             "data-testid":"password-input"
                         }}
@@ -247,8 +230,10 @@ const RegistryForm = () =>{
                         type="password"
                         value={confirmPassword}
                         onChange={handleConfirmPassword}
-                        error={passwordMatchError}
-                        helperText={passwordMatchError && "Passwords do not match"}
+                        //tem de ser no submit para so aparecer quando se quer 
+                        error={formSubmitted && !passwordConfirmation(user.password, confirmPassword)}
+                        
+                        helperText={formSubmitted && !passwordConfirmation(user.password, confirmPassword) && "Passwords do not match"}
                         inputProps={{
                             "data-testid":"confirm-password-input"
                         }}
@@ -258,6 +243,7 @@ const RegistryForm = () =>{
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
+                        data-testid="register-button"
                     >
                         Register
                     </Button>
@@ -267,6 +253,7 @@ const RegistryForm = () =>{
                         fullWidth
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
+                        data-testid="return-login-button"
                     >
                         return to login page
                     </Button>
@@ -277,3 +264,4 @@ const RegistryForm = () =>{
 };
 
 export default RegistryForm;
+
--- src/pages/Registry/index.test.js
+++ src/pages/Registry/index.test.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { render, fireEvent, act, getByTestId } from '@testing-library/react';
-import RegistryForm from './index.js';
+import { render, fireEvent } from '@testing-library/react';
+import RegistryForm from './index';
+import { passwordConfirmation } from './index'; 
+import { credentialConfirmation } from './index';
+import { emailValidation } from './index';
+import { passwordValidation } from './index';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '@testing-library/jest-dom/extend-expect';
+import { toBeChecked } from '@testing-library/jest-dom/matchers';
+import { Mail } from '@mui/icons-material';
+import * as FormFunctions from './index';
+
+
+
 
 describe('RegistryForm', () => { //group related tests together, in this case is the registry form 
+    const registryForm = require('./index')
   test('renders all form fields', () => {//aqui etá a testar se os campos do formulario são renderizados
-    const { getByTestId } = render(<Router>
+    const { getByTestId } = render(
+    <Router>
         <RegistryForm />
-      </Router>);
+    </Router>);
     //espera que os campos com os nomes que foram dados estejam no documento
     expect(getByTestId("first-name-input")).toBeInTheDocument();
     expect(getByTestId("last-name-input")).toBeInTheDocument();
@@ -19,9 +31,10 @@ describe('RegistryForm', () => { //group related tests together, in this case is
   });
 
   test('allows user input and updates state', () => {//aqui testa se os inputs alteram os valores e são atualizados
-    const { getByTestId } = render(<Router>
+    const { getByTestId } = render(
+    <Router>
         <RegistryForm />
-      </Router>);
+    </Router>);
     //fire event simula o utilizador a escrever no campo, a simular um evento 
     fireEvent.change(getByTestId("first-name-input"), { target: { value: 'John' } });
     fireEvent.change(getByTestId("last-name-input"), { target: { value: 'Doe' } });
@@ -29,6 +42,7 @@ describe('RegistryForm', () => { //group related tests together, in this case is
     fireEvent.change(getByTestId("email-input"), { target: { value: 'john@example.com' } });
     fireEvent.change(getByTestId("password-input"), { target: { value: 'password' } });
     fireEvent.change(getByTestId("confirm-password-input"), { target: { value: 'password' } });
+    
     
     //aqui espera que o resultado do evento tenha o valor que foi testado 
     expect(getByTestId("first-name-input")).toHaveValue('John');
@@ -40,55 +54,148 @@ describe('RegistryForm', () => { //group related tests together, in this case is
   });
 
   /*ESTES TESTES AINDA NAO FUNCIONAM*/ 
-
-  //teste de email invalido
-  test('displays error message for invalid email', () => {
-    const { getByTestId, getByText } = render(
-      <Router>
-        <RegistryForm />
-      </Router>
-    );
-
-    fireEvent.change(getByTestId("email-input"), { target: { value: 'invalidemail' } });
-    
-    expect(getByText(/invalid email/i)).toBeInTheDocument();
-  });
-
-  //teste de password invalida
-  test('displays error message for invalid password', () => {
-    const { getByTestId, getByText } = render(
-      <Router>
-        <RegistryForm />
-      </Router>
-    );
-
-    fireEvent.change(getByTestId("password-input"), { target: { value: 'pass' } });
-    
-    expect(getByText(/invalid password/i)).toBeInTheDocument();
-  });
-
-  //teste de confirmar password não estar de acordo com a password
-  test('displays error message for mismatched passwords', () => {
-    const passwordConfirmationMock = jest.fn((password, confirmPassword)=>{
-        if (password !== confirmPassword){
-            return true;
-        }
-        return false;
+describe('When credentials are invalid function values return false', ()=>{
+    //teste de email invalido
+    test('Invalid email return false on email validation', ()=>{
+        expect(emailValidation('mail invalido')).toBe(false);
+        expect(emailValidation('mailinvalido.com')).toBe(false);
+        expect(emailValidation('validEmail@Mail.com')).toBe(true);
     });
 
-    const { getByTestId } = render(
-      <Router>
-        <RegistryForm passwordConfirmation={passwordConfirmationMock} />
-      </Router>
-    );
-    
-    //criar jest.fn aqui 
-    //chamar uma mock fn que seja triggered pelo input
-    //neste caso seria 
+    //testes de função confirm password. Fazer para as outras funções como email validation, password validation e credential validation
+    test('confirms password correctly', () => {
+        // Test passwordConfirmation function with matching passwords
+        expect(passwordConfirmation('password', 'password')).toBe(true);
+        // Test passwordConfirmation function with mismatched passwords
+        expect(passwordConfirmation('password', 'differentpassword')).toBe(false);
+    });
 
-    fireEvent.change(getByTestId("password-input"), { target: { value: 'password' } });
-    fireEvent.change(getByTestId("confirm-password-input"), { target: { value: 'differentpassword' } });
-    expect(passwordConfirmationMock).toHaveBeenCalled();
-    //expect(getByText(/Passwords do not match/i)).toBeInTheDocument();
-  });
+    test('Credential confirmation returns true with correct credentials and false otherwise', ()=>{
+        expect(credentialConfirmation('password', 'password', 'valid@mail.com')).toBe(true);
+        expect(credentialConfirmation('password', 'Differentpassword', 'invalid mail.com')).toBe(false);
+
+    })
+
+    test('password validation returns true when meets criteria and false otherwise',()=>{
+        expect(passwordValidation('pass')).toBe(false);
+        expect(passwordValidation('password')).toBe(true);
+    })
+
+
+    
+})
+
+
+describe ('functions within submits are called when submit credentials is clicked', ()=>{
+
+    test('calls confirm credentials function when handle submit is clicked', () => {
+        const handleSubmitSpy = jest.spyOn(registryForm, 'credentialConfirmation').mockReturnValue(true);
+        const {getByTestId} = render(
+            <Router>
+              <RegistryForm />
+            </Router>
+        )
+        fireEvent.submit(getByTestId('register-button'));
+        expect(handleSubmitSpy).toHaveBeenCalled();
+        handleSubmitSpy.mockRestore();
+        
+      });
+      /************************************* */
+    
+      //teste de password invalida
+        const invalidPasswordMock = jest.fn ();
+      // Mock fireEvent.change to check password length
+        jest.spyOn(fireEvent, 'change').mockImplementation((element, value) => {
+        if (element.getAttribute('data-testid') === 'password-input' && value.target.value.length < 8) {
+        invalidPasswordMock();
+        }
+        });
+    
+      test('displays error message for invalid password', () => {
+         const { getByTestId, getByText } = render(
+          <Router>
+            <RegistryForm />
+          </Router>
+        );
+    
+        fireEvent.change(getByTestId("password-input"), { target: { value: 'pass' } });
+        
+        //expect(getByText(/invalid password/i)).toBeInTheDocument();
+        expect(invalidPasswordMock).toHaveBeenCalled();
+      });
+    
+      //teste de confirmar password não estar de acordo com a password
+    
+      
+      test('displays error message for mismatched passwords', () => {
+        const { getByTestId} = render(
+          <Router>
+            <RegistryForm />
+          </Router>
+        );
+    
+        fireEvent.change(getByTestId("password-input"), { target: { value: 'password' } });
+        fireEvent.change(getByTestId("confirm-password-input"), { target: { value: 'differentpassword' } });
+        //tou a fazer isto mal porque espero que apenas ao mudar os valores isto seja chamado
+        //tenho de fazer um mock ao submit e aí esperar que esta função seja chamada
+        expect(registryForm.passwordConfirmation).toHaveBeenCalledTimes(1);
+      });
+
+})
+
+describe ('When buttons are clicked the page navigates', ()=>{
+    const mockNavigate = jest.fn();
+    
+    // Mock the useNavigate hook
+    jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockNavigate,
+    }));
+
+    test('calls navigate function when registration is successful', () => {
+        const { getByTestId } = render(
+        <Router>
+            <RegistryForm />
+        </Router>
+        );
+        //fazer spy on no navigate
+        //esta não funciona porque não chamamos o botao direto, estamos a chamar a função handle submit que depois é que redireciona
+
+        // Simulate filling out the registration form
+        fireEvent.change(getByTestId("first-name-input"), { target: { value: 'John' } });
+        fireEvent.change(getByTestId("last-name-input"), { target: { value: 'Doe' } });
+        fireEvent.change(getByTestId("email-input"), { target: { value: 'john.doe@example.com' } });
+        fireEvent.change(getByTestId("password-input"), { target: { value: 'password' } });
+        fireEvent.change(getByTestId("confirm-password-input"), { target: { value: 'password' } });
+
+        // Submit the form
+        fireEvent.click(getByTestId("register-button"));
+
+        // Check if the navigate function is called with the correct argument
+        //expect(mockNavigate).toHaveBeenCalledWith('/login', { replace: true });
+        expect(mockNavigate).toHaveBeenCalled();
+    });
+
+
+    test('Goes to login page when user wants to return to login', async ()=>{
+        const {getByTestId} = render (
+        <Router>
+            <RegistryForm />
+        </Router>
+        );
+
+        fireEvent.click(getByTestId("return-login-button"));
+        console.log('Button clicked');
+        await new Promise(resolve => setTimeout(resolve, 100));
+        console.log('mockNavigate called:', mockNavigate.mock.calls);
+
+        //expect(mockNavigate).toHaveBeenCalledWith('/login', { replace: true })
+        expect(mockNavigate).toHaveBeenCalled(); //este chama
+        //o problema neste teste é que chama o navigate, mas eu quero que seja especifico no navigate que está a chamar
+    })
+})
+
+//fazer uma describe das funções chamadas
+
+
 });
