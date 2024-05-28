cdadf2fa4438343766f033fa3f1163ba6ffaf47b
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-05-28 11:21:11 +0100

    criou-se o componente carrousel e o ficheiro para project div

---
+++ src/components/Caroussel/index.js
@@ -0,0 +1,12 @@
+//este componente carrosel recebe um array de projects como props
+//que por sua vez cada projeto (que sera um objecto) tem uma estrutura definida que é chamada 
+import react from 'react'
+import Carousel from 'react-material-ui-carousel'
+
+const Carousel= ({props})=>{
+    return (
+        <Carousel>
+        </Carousel>
+    )
+
+}
