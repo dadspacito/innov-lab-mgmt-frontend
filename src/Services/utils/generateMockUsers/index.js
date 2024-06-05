8367c634748fdbb7457cdad94eac7e94c24408fa
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-06-05 12:00:49 +0100

    admin user to test login and other conditionals that require a user

---
+++ src/Services/utils/generateMockUsers/index.js
@@ -0,0 +1,21 @@
+/**
+ * atributos:{
+ * nome, username, email, pass, token (componentes iniciais para fazer login )
+ * foto, location, 
+ * }
+ */
+export const mockUser=()=>{
+    return [
+        {
+        id:1,
+        name:'admin',
+        email:'admin@mail.com',
+        password:'adminPass',
+        location:'Tomar',
+        }
+        
+
+    ]
+    
+
+}
