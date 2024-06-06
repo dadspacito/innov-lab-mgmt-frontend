f2e08ed260b8b92abf470c1502f6cd9e1cfbda8e
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-06-06 15:45:30 +0100

    User profile with user components

---
+++ src/pages/UserProfile/index.js
@@ -0,0 +1,4 @@
+//vai receber os componentes associados ao perfil do user
+//recebe um header do perfil que mostra a foto e os detalhes do user, tal como a biografia- UserHeader component
+//recebe um preferences do user, onde mostra as suas skills e interesses, tal como a preferencia de ter um perfil privado ou publico- UserPreferences component
+//recebe os projetos onde este user está inserido, em forma de tabela-UserProjects component
