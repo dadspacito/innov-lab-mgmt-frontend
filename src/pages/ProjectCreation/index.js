8d725a123f63c0b3a4b5f11208008598c41b1f2a
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-06-06 10:53:24 +0100

    criou-se o passo da adição das keywords

--- src/pages/ProjectCreation/index.js
+++ src/pages/ProjectCreation/index.js
@@ -19,6 +19,7 @@ const ProjectCreationWizard = ()=>{
         location:'',//este é uma int/id de uma localização da tabela de localizações
         keywords:[],//são as tags que depois se associam as chips dos projetos
         members:[],//array de ids atraves de users disponiveis em base de dados (que não estejam em projetos )
+        //depois da adição dos membros, perguntar se quer gravar o projeto 
         executionPlan:'',//plano de execução, pode sair mais tarde
         materials:[], //ids de materiais da tabela de materiais 
         //criador
