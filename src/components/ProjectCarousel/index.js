c2f7e5aeae4caeb411782a85aef3d457c68df393
Pedro Monteiro <pmdmonteiro1992@gmail.com>
2024-06-08 17:29:25 +0100

    alteração do carrousel para um mais robusto (react-slick)

--- src/components/ProjectCarousel/index.js
+++ src/components/ProjectCarousel/index.js
@@ -1,31 +1,66 @@
-//este componente carrosel recebe um array de projects como props
-//que por sua vez cada projeto (que sera um objecto) tem uma estrutura definida que é chamada 
-
-import React from 'react'
-import Carousel from 'react-material-ui-carousel'
-import ProjectDiv from '../projectDiv'
+import React from 'react';
+import Slider from 'react-slick';
+import PropTypes from 'prop-types';
+import ProjectDiv from '../projectDiv'; // Ensure the correct path to ProjectDiv
 import './style.css'
 
-//tenho de importar o style desta pagina para o styling do project div ser aqui
-const ProjectCarousel= ({projects})=>{
-    
-    /*ProjectCarousel.propTypes={
-        projects:PropTypes.arrayOf(
-            PropTypes.shape({
-                title: PropTypes.string.isRequired,
-                Description: PropTypes.string.isRequired
-            })
-        )
-    }*/
-    return (
-        <Carousel className="carouselContainer">
-            {projects.map(project=>(
-                <div key={project.id} className="projectWrapper">
-                <ProjectDiv title={project.title} description={project.description} />
-              </div>
-            ))}
-        </Carousel>
-    )
 
+const ProjectCarousel = ({ projects }) => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        centerMode:true,
+        centerPadding:'0px',
+        slidesToShow: 3, // Show 3 projects at a time
+        slidesToScroll: 1, // Scroll one slide at a time
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    return (
+        <Slider {...settings} className="carouselContainer">
+            {projects.map(project => (
+                <div key={project.id} className="projectWrapper">
+                    <ProjectDiv title={project.title} description={project.description} />
+                </div>
+            ))}
+        </Slider>
+    );
 }
-export default ProjectCarousel
+
+ProjectCarousel.propTypes = {
+    projects: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired, // Ensure every project has a unique id
+            title: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+        })
+    ).isRequired,
+};
+
+export default ProjectCarousel;
